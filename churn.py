b88c189dbfa7c8bdf9d8b45e57ce232e26537855
FrugoFruit90 <janzysko@gmail.com>
2016-09-30 00:35:18 +0200

    Logistic regression

--- churn.py
+++ churn.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import roc_curve, auc
 
 # Load the data
 X = pd.read_csv('churn.csv', ';')
@@ -15,8 +17,13 @@ y = np.where(X['Churn?'] == 'True.', 1, 0)
 X.drop(['State', 'Area Code', 'Phone', 'Churn?'], axis=1, inplace=True)
 X["Int'l Plan"] = X["Int'l Plan"] == 'yes'
 X["VMail Plan"] = X["VMail Plan"] == 'yes'
-print(X.ix[2])
+# print(X.ix[2])
 X = StandardScaler().fit_transform(X)
+# print(X[0])
 
-
-
+clf = LogisticRegressionCV(cv=10, scoring='roc_auc', n_jobs=-1)
+clf.fit(X, y)
+y_hat = clf.predict(X)
+print('CV scores: %s' % np.mean(clf.scores_[1]))
+false_positive_rate, true_positive_rate, thresholds = roc_curve(y, y_hat)
+print('AUC: %f' % auc(false_positive_rate, true_positive_rate))
